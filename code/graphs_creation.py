e028a123aeb830b4fe8bd32290325b29fcfae3f8
thibui0908 <thi.t.bui@sjsu.edu>
2022-10-31 16:17:46 -0700

    more data files 10/31

--- code/graphs_creation.py
+++ code/graphs_creation.py
@@ -1,41 +1,95 @@
 import csv
 from math import degrees
+from numpy import average
 import pandas as pd
 import networkx as nx
+import os
+import re
 
-with open('./data/real_bot_user_nodes_0.csv', newline='') as f:
-    reader = csv.reader(f)
-    node_list = list(reader)
+directory_edge = './data/bot_user_edge'
+directory_node = './data/bot_user_nodes'
 
-print(len(node_list))
+files_edge = [file for file in os.listdir(directory_edge)]
+files_node = [file for file in os.listdir(directory_node)]
 
-G = nx.Graph()
+number_of_nodes = 0
+number_of_edges = 0
+number_isolated = 0
+density = 0
+max_degree = 0
+min_degree = 0
+average_degree = 0
+connected_component = 0
+count = 0
 
-for i in node_list:
-    G.add_node(int(i[0]))
+def social_network_analysis(edge_file, node_file):
+    node_reader = csv.reader(node_file)
+    node_list = list(node_reader)
 
+    G = nx.Graph()
 
-with open('./data/real_bot_user_edge_0.csv', newline='') as f:
-    reader = csv.reader(f)
-    edges = list(reader)
+    for i in node_list:
+        G.add_node(int(i[0]))
 
-for i in edges:
-    G.add_edge(int(i[0]), int(i[1]))
+    edge_reader = csv.reader(edge_file)
+    edge_list = list(edge_reader)
 
-print("Number of nodes: ", G.number_of_nodes())
-print("Number of edges: ", G.number_of_edges())
-print("Number of isolated nodes", len(list(nx.isolates(G))))
-print("Density of a graph: ", nx.density(G))
-
-degrees = sorted(G.degree(), key=lambda x: x[1], reverse=True)
-
-print("Max degree: ", degrees[0][1])
-print("Min degree: ", degrees[len(degrees)-1][1])
-
-sum = 0
-
-for i in degrees:
-    sum = sum + i[1]
+    for i in edge_list:
+        G.add_edge(int(i[0]), int(i[1]))
     
-print("Average degree: ", sum/len(degrees))
-print("Number of connected components: ", nx.number_connected_components(G))
+    res = []
+
+    res.append(G.number_of_nodes())
+    res.append(G.number_of_edges())
+    res.append(len(list(nx.isolates(G))))
+    res.append(nx.density(G))
+    degrees = sorted(G.degree(), key=lambda x: x[1], reverse=True)
+    res.append(degrees[0][1])
+    res.append(degrees[len(degrees)-1][1])
+
+    sum = 0
+
+    for i in degrees:
+        sum = sum + i[1]
+    
+    res.append(sum/len(degrees))
+    
+    res.append(nx.number_connected_components(G))
+    return res
+
+def get_numbers_from_filename(filename):
+    return re.search(r'\d+', filename).group(0)
+
+for filename in files_edge:
+    try:
+        if os.path.getsize(os.path.join(directory_edge,filename)) > 0:
+            with open(os.path.join(directory_edge, filename), 'r', encoding='utf-8') as openfile:
+                print(filename)
+                filenum = get_numbers_from_filename(filename)
+                with open(os.path.join(directory_node, 'bot_user_nodes_{}.csv'.format(filenum)), 'r', encoding='utf-8') as nodefile:
+                    count = count + 1
+                    res = social_network_analysis(openfile, nodefile)
+                    number_of_nodes = number_of_nodes + res[0]
+                    number_of_edges = number_of_edges + res[1]
+                    number_isolated = number_isolated + res[2]
+                    density = density + res[3]
+                    max_degree = max_degree + res[4]
+                    min_degree = min_degree + res[5]
+                    average_degree = average_degree + res[6]
+                    connected_component = connected_component + res[7]
+                    print('File number ', filenum)
+    except Exception as e:
+        print("error:",e)
+
+print("File number ", count)
+print("Number of nodes: ", number_of_nodes/count)
+print("Number of edges: ", number_of_edges/count)
+print("Number of isolated nodes", number_isolated/count)
+print("Density of a graph: ", density/count)
+
+print("Max degree: ", max_degree/count)
+print("Min degree: ", min_degree/count)
+    
+print("Average degree: ", average_degree/count)
+print("Number of connected components: ", connected_component/count)
+
--- code/split_nodes.py
+++ code/split_nodes.py
@@ -1,30 +1,41 @@
-import networkx
 import csv
+import os
+import re 
 
-r_user_file = open("./data/real_bot_user_edge_0.csv")
-
-real_user_file = csv.reader(r_user_file)
-
-print(type(real_user_file))
-
-header = []
-header = next(real_user_file)
-print(header)
-
-nodes = set()
-rows = []
-for row in real_user_file:
+def write_node_file(reader, filenum):
+    print(filename)
+    print(filenum)
+    nodes = set()
+    rows = []
+    for row in reader:
         row[0].strip()
         row[1].strip()
         nodes.add(row[0])
         nodes.add(row[1])
         rows.append(row)
+    print("Number of nodes", len(nodes))
 
-print("Number of nodes", len(nodes))
+    with open('./data/real_user_nodes/real_bot_user_nodes_{}.csv'.format(filenum), 'w') as f:
+        writer = csv.writer(f)
+        for node in nodes:
+            writer.writerow([node])
 
-with open('./data/real_bot_user_nodes_0.csv', 'w') as f:
-    writer = csv.writer(f)
-    for node in nodes:
-        writer.writerow([node])
+def get_numbers_from_filename(filename):
+    return re.search(r'\d+', filename).group(0)
 
+directory = './data/bot_user_edge'
+
+files = [file for file in os.listdir(directory)]
+print(len(files))
+
+for filename in files:
+    try:
+        if os.path.getsize(os.path.join(directory,filename)) > 0:
+            with open(os.path.join(directory,filename), 'r') as openfile:
+                reader = csv.reader(openfile)
+                print(filename)
+                write_node_file(reader, get_numbers_from_filename(filename))
+    except Exception as e:
+        print(filename)
+        print("error:",e)
 
